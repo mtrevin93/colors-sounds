943a6bb1e68b10320180bedfb4f31520d46a2edc
Michael Trevino <mtrevin93@gmail.com>
2021-09-02 11:56:51 -0400

    objects initialized

--- colors-sounds/Program.cs
+++ colors-sounds/Program.cs
@@ -4,13 +4,10 @@
     {
         static void Main(string[] args) 
         {
-            Zero fxs = new Zero();
-            Tesla modelS = new Tesla();
-            Cessna mx410 = new Cessna();
-
-            fxs.Drive();
-            modelS.Drive();
-            mx410.Drive();
+            Zero fxs = new Zero() {BatteryKWh = 100.5, MainColor = 'Blue', MaximumOccupancy = 2};
+            Tesla modelS = new Tesla() {BatteryKWh = 200.5, MainColor = 'Black' MaximumOccupancy =2};
+            Cessna mx410 = new Cessna() {FuelCapacity = 300.2, MainColor = 'Red' MaximumOccupancy =4};
+            Ram ramThing = new Ram() {FuelCapacity = 500.4, MainColor = 'White', MaximumOccupancy = 4};
         }
     }
 }
